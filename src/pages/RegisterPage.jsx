8ecd72dcaf3eedf92862a08bcfcd9d37b793f663
Ospinajuanp <juanpo996@gmail.com>
2025-04-07 11:58:47 -0500

    feature page register, firts fecht with axios

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,15 +1,26 @@
 import { useForm } from 'react-hook-form';
 
+import { registerRequest } from '../api/auth';
+
+import './RegisterPage.css'
+
 const RegisterPage = () => {
 
     const { register, handleSubmit } = useForm();
 
+    const onSubmit = handleSubmit(async (data) => {
+        const res = await registerRequest(data);
+        console.log(res);
+    })
+
     return (
-        <div>
-            <form action="">
-                <input type="text" {...register} />
-                <input type="email" {...register} />
-                <input type="password"{...register} />
+        <div className='register'>
+            <h2 className='register__title'>Register</h2>
+            <form className='register__form' onSubmit={onSubmit}>
+                <input className='register__input register__input--name' type="text" {...register('name', { required: true })} placeholder='name'/>
+                <input className='register__input register__input--email' type="email" {...register('email', { required: true })} placeholder='email'/>
+                <input className='register__input register__input--password' type="password"{...register('password', { required: true })} placeholder='password'/>
+                <button className='register__button' type='submit'>Register</button>
             </form>
         </div>
     );
